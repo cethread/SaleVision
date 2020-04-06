a24bb4e13f87b52ce4d3264bf8a3a5e10f1a2c42
cethread <cethread@aggies.ncat.edu>
2020-04-06 15:44:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let boxStatus = false;
+// let chatButton = document.querySelector('.chatbutton');
+let box = document.querySelector('.chatbox');
+let boxHeader = document.querySelector('.card-header');
+
+
+function openChatBox () {
+    if (boxStatus === false) {
+        box.style.bottom = "0em";
+        boxStatus = true;
+    }
+    else if (boxStatus === true) {
+        box.style.bottom = "-15.6em";
+        boxStatus = false;
+    }
+}
